ff8ea7f99a1df991c2868c0626ceff7082becbbe
kms060616 <2025137021rlaalstjr@g.shingu.ac.kr>
2025-04-04 22:30:37 +0900

    Day 1

---
+++ Assets/Scripts/LevelObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string nextLevel;
+    public void MoveToNextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
